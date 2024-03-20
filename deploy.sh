60bc1a71746ba8c4fdad8d5dde86662ff9e4911a
andrewk <A-Kuzminetskiy@yugcontract.ua>
2024-03-20 14:46:04 +0200

    fix

--- deploy.sh
+++ deploy.sh
@@ -9,8 +9,8 @@ docker rm tms-vue-prod
 
 docker-compose up -d --build tms-vue-prod
 
-docker kill tms-api-prod
-docker rm tms-api-prod
+#docker kill tms-api-prod
+#docker rm tms-api-prod
 
-docker-compose up -d --build tms-api-prod
+#docker-compose up -d --build tms-api-prod
 
