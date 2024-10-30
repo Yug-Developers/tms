410c0ca2f0403608317c4c27f115fd06bd8a52fb
andrewk <A-Kuzminetskiy@yugcontract.ua>
2024-10-30 16:32:02 +0200

    Update CouchDB remote URL configuration and increment package version to 0.1.5

--- api/Config.js
+++ api/Config.js
@@ -1,7 +1,6 @@
 module.exports = {
-    remoteCouchDb: process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'dev_production' ? 
-    'https://dev.sharp.com.ua/' : 'http://dev.tms.yugcontract.ua:5984/',
-
+    remoteCouchDb: process.env.NODE_ENV === 'production' ? 'https://tms-db.yugcontract.ua/' :  
+    (process.env.NODE_ENV === 'dev_production' ? 'https://dev.tms-db.yugcontract.ua/' : 'http://dev.tms.yugcontract.ua:5984/'),
     foxtrotOauth2: {
         client_id: 'yugcontract-client',
         secret: 'd260a147-bada-4f04-9e6f-639f31fdd736',
